f53f02f1d1296aa7fe478d1266463e8cefc692ac
Parker Lougheed <parlough@gmail.com>
2025-06-29 04:36:49 +0800

    Prepare for 0.1.0 release

--- lib/unified_diff.dart
+++ lib/unified_diff.dart
@@ -1,5 +1,8 @@
 /// Generate the unified diff between two lists of strings,
 /// often representing the contents of two files.
+@experimental
 library;
+
+import 'package:meta/meta.dart';
 
 export 'src/unified_diff.dart';
